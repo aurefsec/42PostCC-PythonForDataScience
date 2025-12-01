4d1c9ccff8fbf1e61a336ab18da8dcf0f6befe9c
aurefsec <auref.contact@protonmail.com>
2025-12-01 03:15:42 +0100

    ex00

---
+++ Python-3-OOP/ex00/S1E9.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Abstract class for all characters"""
+
+    # Methods
+    @abstractmethod
+    def __init__(self, first_name, is_alive=True):  # is_alive is non mandatory
+        """Parent init"""
+        pass  # Attributes will be init from child
+
+    def die(self):
+        """Kill the character if he's alive"""
+        if self.is_alive is True:
+            self.is_alive = False
+
+
+class Stark(Character):
+    """A character that inherits from Character abstract class """
+
+    # Methods
+    def __init__(self, first_name, is_alive=True):
+        """Child init"""
+        self.first_name = first_name
+        self.is_alive = is_alive
