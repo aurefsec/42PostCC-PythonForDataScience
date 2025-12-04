58e3e1e8370c391000854423456fb986d97b0c64
aurefsec <auref.contact@protonmail.com>
2025-12-04 21:07:58 +0100

    ex01

---
+++ Python-4-Dod/ex01/in_out.py
@@ -0,0 +1,25 @@
+def square(x: int | float) -> int | float:
+    """Return a square of int or float"""
+    return x ** 2
+
+
+def pow(x: int | float) -> int | float:
+    """Return a power of int or float"""
+    return x ** x
+
+
+def outer(x: int | float, function) -> object:
+    """Return a function that applies function to a stored value (closure)"""
+
+    count = 0
+
+    def inner() -> float:
+        """Applies 'function' to the stores value, updates and returns it"""
+
+        nonlocal count  # 'nonlocal' to use count
+        if count == 0:
+            count = x
+        count = function(count)
+        return count
+
+    return inner
